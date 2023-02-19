37a25f377eb90e8c40537cc4b2222870bd8ba4db
armanfkt <armanfakhte@gmail.com>
2023-02-19 21:56:25 +0330

    fff

---
+++ components/contact_us/contact_us.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+             <nuxt-link to="/homepage">Homepage</nuxt-link>
+            </v-col>
+            
+        </v-row>
+    </v-container>
+</template>
+<script>
+export default{
+    name:"contact_us"
+}
+</script>
---
+++ pages/contact_us/index.vue
@@ -0,0 +1,19 @@
+
+import Contact_us from '../../components/contact_us/contact_us.vue';
+
+<template>
+    <Contact_us/>
+</template>
+
+<script>
+ import Contact_us  from "comments/contact_us/contact_us.vue"
+
+ export default{
+    name:"contact",
+
+    components:{
+        Contact_us
+    }
+    
+    }
+</script>
